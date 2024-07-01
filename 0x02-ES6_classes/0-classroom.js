b7bfa154d4c540f70ad3c4896d83d3116665b95a
2100032578cse <uwizeyesandri@gmail.com>
2024-07-01 09:13:30 +0000

    fixing task 0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,13 +1,13 @@
 /**
- * Represents a class room.
+ * Represents a default class room.
  */
 export default class ClassRoom {
-    /**
+  /**
      * Creates a new @see {@link ClassRoom}.
      * @param {Number} maxStudentsSize - The maximum number of
      * students in the class.
      */
-    constructor(maxStudentsSize) {
-      this._maxStudentsSize = maxStudentsSize;
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
   }
+}
