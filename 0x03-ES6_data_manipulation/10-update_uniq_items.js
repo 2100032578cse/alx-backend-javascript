00af0edf62ec0491d8b4fccb54a4ae3cb8651ca1
2100032578cse <uwizeyesandri@gmail.com>
2024-07-03 13:32:59 +0000

    fixing js style

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -5,12 +5,12 @@
  * @author Uwizeyimana Sandrine <https://github.com/2100032578cse>
  */
 export default function updateUniqueItems(map) {
-    if (!(map instanceof Map)) {
-      throw new Error('Cannot process');
-    }
-    map.forEach((value, key) => {
-      if (value === 1) {
-        map.set(key, 100);
-      }
-    });
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
   }
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+}
