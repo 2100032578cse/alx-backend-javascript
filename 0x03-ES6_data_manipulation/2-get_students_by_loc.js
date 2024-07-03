1ed7bbf1a9686ab323e7600914a819a66f1a5672
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2024-07-03 19:20:14 +0530

    task2 fix

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,14 +1,14 @@
 /**
- * @author Uwizeyimana Sandrine <https://github.com/2100032578cse>
  * Retrieves students in a given location.
  * @param {{
-*   id: Number,
-*   firstName: String,
-*   location: String
-* }[]} students - The list of students.
-* @param {String} city - The location.
-* @returns
-*/
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @param {String} city - The location.
+ * @author Uwizeyimana Sandrine <https://github.com/2100032578cse>
+ * @returns
+ */
 export default function getStudentsByLocation(students, city) {
   if (students instanceof Array) {
     return students.filter((student) => student.location === city);
