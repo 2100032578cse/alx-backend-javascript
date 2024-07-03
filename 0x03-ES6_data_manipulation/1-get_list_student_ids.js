fe24d82953dcf88682f85f7c3702f808f717dfe2
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2024-07-03 19:09:00 +0530

    task1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,13 +1,13 @@
 /**
- * @author Uwizeyimana Sandrine <https://github.com/2100032578cse>
  * Retrieves ids from a list of students.
  * @param {{
-*   id: Number,
-*   firstName: String,
-*   location: String
-* }[]} students - The list of students.
-* @returns
-*/
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns
+ */
 export default function getListStudentIds(students) {
   if (students instanceof Array) {
     return students.map((student) => student.id);
